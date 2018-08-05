826728aeb951282b19695f18672399d016843692
Maxim Dzabraev <dzabraew@gmail.com>
2018-08-05 14:05:47 +0300

    ad missing ;

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@ with import <nixpkgs> {};
         sha256 = "0n0301rjhkbgyspb1ar1g782jr8596j7bwf6zbndy48ck199fdda";
       };
       description = "Non aligned original dataset.";
-    }
+    };
     #media = {
     #  url = http://10.xxx.xxx.183/datasets/media.tar;
     #  sha256 = "";
